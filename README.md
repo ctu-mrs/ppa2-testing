b245c8fe29c2e4423d827169b1b404a8c4d0f11b
Tomas Baca <Tomas Baca>
2025-03-12 07:50:20 +0100

    refactor the name

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
-# PPA2 Unstable
+# PPA2 Testing
 
-Personal Package Archive (PPA) for unstable ROS2 MRS deb packages.
+Personal Package Archive (PPA) for testing ROS2 MRS deb packages.
 
 ## Adding the PPA
 
 ```bash
-curl https://ctu-mrs.github.io/ppa2-unstable/add_ppa.sh | bash
+curl https://ctu-mrs.github.io/ppa2-testing/add_ppa.sh | bash
 ```
 
 ## Uninstalling packages from this PPA
 
 ```bash
 sudo apt-get -y install aptitude
-sudo apt remove $(aptitude search -F '%p' '~S ~i ?origin("ctu-mrs") ?label("unstable")')
+sudo apt remove $(aptitude search -F '%p' '~S ~i ?origin("ctu-mrs") ?label("testing")')
 ```
 
 ## Removing the PPA
 
 ```bash
-curl https://ctu-mrs.github.io/ppa2-unstable/remove_ppa.sh | bash
+curl https://ctu-mrs.github.io/ppa2-testing/remove_ppa.sh | bash
 ```
--- resources/scripts/add_ppa.sh
+++ resources/scripts/add_ppa.sh
@@ -5,18 +5,18 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
-echo "$0: Adding MRS ROS2 Unstable PPA repository"
+echo "$0: Adding MRS ROS2 Testing PPA repository"
 
 sudo apt-get -y install curl gpg dpkg-dev
 
 ARCH=$(dpkg-architecture -qDEB_HOST_ARCH)
 
-curl -s --compressed "https://ctu-mrs.github.io/ppa2-unstable/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg >/dev/null
-sudo curl -s --compressed -o /etc/apt/sources.list.d/ctu-mrs-unstable.list "https://ctu-mrs.github.io/ppa2-unstable/ctu-mrs-apt.list"
-sudo curl -s --compressed -o /etc/apt/preferences.d/ctu-mrs-unstable-preferences "https://ctu-mrs.github.io/ppa2-unstable/ctu-mrs-ppa-preferences.txt"
-sudo curl -s --compressed -o /etc/ros/rosdep/sources.list.d/ctu-mrs-unstable.list "https://ctu-mrs.github.io/ppa2-unstable/ctu-mrs-$ARCH.list"
+curl -s --compressed "https://ctu-mrs.github.io/ppa2-testing/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg >/dev/null
+sudo curl -s --compressed -o /etc/apt/sources.list.d/ctu-mrs-testing.list "https://ctu-mrs.github.io/ppa2-testing/ctu-mrs-apt.list"
+sudo curl -s --compressed -o /etc/apt/preferences.d/ctu-mrs-testing-preferences "https://ctu-mrs.github.io/ppa2-testing/ctu-mrs-ppa-preferences.txt"
+sudo curl -s --compressed -o /etc/ros/rosdep/sources.list.d/ctu-mrs-testing.list "https://ctu-mrs.github.io/ppa2-testing/ctu-mrs-$ARCH.list"
 sudo apt-get -y update
 
 rosdep update
 
-echo "$0: Finished adding MRS ROS2 Unstable PPA repository"
+echo "$0: Finished adding MRS ROS2 Testing PPA repository"
