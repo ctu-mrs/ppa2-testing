1051757a731a96c9cc799676f043d24c8fbe6b1e
Filip <4060824+filipopo@users.noreply.github.com>
2026-07-22 11:39:21 +0200

    Remove -y from apt update (does nothing)

--- resources/scripts/add_ppa.sh
+++ resources/scripts/add_ppa.sh
@@ -19,7 +19,7 @@ curl -s --compressed "https://ctu-mrs.github.io/ppa2-testing/ctu-mrs.gpg" | gpg 
 sudo curl -s --compressed -o /etc/apt/sources.list.d/ctu-mrs-testing.list "https://ctu-mrs.github.io/ppa2-testing/ctu-mrs-apt.list"
 sudo curl -s --compressed -o /etc/apt/preferences.d/ctu-mrs-testing-preferences "https://ctu-mrs.github.io/ppa2-testing/ctu-mrs-ppa-preferences.txt"
 sudo curl -s --compressed -o /etc/ros/rosdep/sources.list.d/ctu-mrs-testing.list "https://ctu-mrs.github.io/ppa2-testing/ctu-mrs-$ARCH.list"
-sudo apt-get -y update
+sudo apt-get update
 
 rosdep update
 
--- resources/scripts/remove_ppa.sh
+++ resources/scripts/remove_ppa.sh
@@ -6,7 +6,7 @@ sudo rm /etc/apt/sources.list.d/ctu-mrs-testing.list
 sudo rm /etc/apt/preferences.d/ctu-mrs-testing-preferences
 sudo rm /etc/ros/rosdep/sources.list.d/ctu-mrs-testing.list
 
-sudo apt-get -y update
+sudo apt-get update
 rosdep update
 
 echo "$0: Finished removing MRS ROS2 Testing PPA repository"
