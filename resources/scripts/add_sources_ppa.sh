f7d2d711cd5f6688ade3fe092f111b320cbfa02a
Filip <4060824+filipopo@users.noreply.github.com>
2026-08-05 12:27:39 +0200

    Shorten flags

--- resources/scripts/add_sources_ppa.sh
+++ resources/scripts/add_sources_ppa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors "https://ctu-mrs.github.io/ppa2-testing/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg > /dev/null
-sudo curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors -o /etc/apt/sources.list.d/ctu-mrs-testing.list "https://ctu-mrs.github.io/ppa2-testing/ctu-mrs-apt.list"
-sudo curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors -o /etc/apt/preferences.d/ctu-mrs-testing-preferences "https://ctu-mrs.github.io/ppa2-testing/ctu-mrs-ppa-preferences.txt"
+curl -s --compressed --retry 4 --retry-all-errors "https://ctu-mrs.github.io/ppa2-testing/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg > /dev/null
+sudo curl -s --compressed --retry 4 --retry-all-errors -o /etc/apt/sources.list.d/ctu-mrs-testing.list "https://ctu-mrs.github.io/ppa2-testing/ctu-mrs-apt.list"
+sudo curl -s --compressed --retry 4 --retry-all-errors -o /etc/apt/preferences.d/ctu-mrs-testing-preferences "https://ctu-mrs.github.io/ppa2-testing/ctu-mrs-ppa-preferences.txt"
 
-sudo apt-get -o Acquire::Retries="4" update
+sudo apt-get -o Acquire::Retries=4 update
